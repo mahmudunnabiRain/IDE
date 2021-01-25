82108bc2053f66de458ac3035fa9f14d2617415f
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-01-25 12:06:08 +0600

    file open, save, close, run implemented

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import tkinter as tk
 import tkinter.font as font
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 window = tk.Tk()
-window.title("IDE")
+window.title("Python IDE - Untitled")
+window.iconbitmap("python_icon.ico")
 
 buttonFont = font.Font(weight="bold", size=10)
 textBoxFont = font.Font(family="Monaco", size=12)
+terminalFont = font.Font(family="Monaco", size=10)
+
 
 frame1 = tk.Frame(master=window,
                   relief=tk.FLAT,
                   borderwidth=1,
-                  height=30,
-                  width=400,)
+                  height=30,)
 
 frame2 = tk.Frame(master=window,
                   relief=tk.FLAT,
-                  borderwidth=1,
-                  height=40,
-                  width=400,)
+                  borderwidth=1
+                  )
 
 
 def handle_click_open():
-    print("Open button was clicked!")
     """Open a file for editing."""
     filepath = askopenfilename(
-        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+        filetypes=[("Python Files", "*.py"), ("All Files", "*.*")]
     )
     if not filepath:
         return
@@ -33,7 +33,7 @@ def handle_click_open():
     with open(filepath, "r") as input_file:
         text = input_file.read()
         textBox.insert(tk.END, text)
-    window.title(f"Simple Text Editor - {filepath}")
+    window.title(f"Python IDE - {filepath}")
 
 
 openButton = tk.Button(text="Open",
@@ -47,7 +47,21 @@ openButton = tk.Button(text="Open",
 
 
 def handle_click_save():
-    print("Save button was clicked!")
+    """Save the current file as a new file."""
+    filepath = window.title()[13:len(window.title())]
+    if filepath == "Untitled":
+        filepath = asksaveasfilename(
+            defaultextension="py",
+            filetypes=[("Python Files", "*.py"), ("All Files", "*.*")],
+        )
+        if not filepath:
+            return
+    else:
+        filepath = window.title()[13:len(window.title())]
+    with open(filepath, "w") as output_file:
+        text = textBox.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Python IDE - {filepath}")
 
 
 saveButton = tk.Button(text="Save",
@@ -61,7 +75,12 @@ saveButton = tk.Button(text="Save",
 
 
 def handle_click_run():
-    print("Run button was clicked!")
+    handle_click_save()
+    filepath = window.title()[13:len(window.title())]
+    if filepath != "Untitled":
+        command = 'cmd /c "py {}"' .format(filepath)
+        print(command)
+        # os.system(command)
 
 
 runButton = tk.Button(text="Run",
@@ -73,16 +92,43 @@ runButton = tk.Button(text="Run",
                       command=handle_click_run,
                       master=frame1)
 
+
+def handle_click_close():
+    textBox.delete("1.0", tk.END)
+    window.title("Python IDE - Untitled")
+
+
+closeButton = tk.Button(text="Close",
+                        width=6,
+                        height=1,
+                        bg="orange",
+                        fg="Black",
+                        font=buttonFont,
+                        command=handle_click_close,
+                        master=frame1)
+
+
 textBox = tk.Text(master=frame2,
-                  font=textBoxFont)
+                  font=textBoxFont,
+                  height=24,
+                  width=60)
+
+terminal = tk.Text(master=frame2,
+                   font=terminalFont,
+                   height=8,
+                   width=60)
+
 
 frame1.pack(fill=tk.X, side=tk.TOP)
 openButton.place(x=0, y=0)
 saveButton.place(x=60, y=0)
 runButton.place(x=120, y=0)
+closeButton.place(x=180, y=0)
 
-frame2.pack(fill=tk.BOTH, expand=True)
 textBox.pack(fill=tk.BOTH, expand=True)
+terminal.pack(fill=tk.BOTH, expand=True)
+frame2.pack(fill=tk.BOTH, expand=True)
 
-
+window.update()
+window.minsize(window.winfo_width(), window.winfo_height())
 window.mainloop()
